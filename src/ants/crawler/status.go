48dc05ac86b1f92e30a155d15b4a276ba0635a6e
wcong <wc19920415@gmail.com>
2015-03-22 15:12:04 +0800

    export crawl status bug

--- src/ants/crawler/status.go
+++ src/ants/crawler/status.go
@@ -28,8 +28,8 @@ func NewSpiderStatus(name string) *SpiderStatus {
 // status of crawler
 // crawled spiders and running spiders
 type CrawlerStatus struct {
-	crawledSpider []*SpiderStatus
-	runningSpider map[string]*SpiderStatus
+	CrawledSpider []*SpiderStatus
+	RunningSpider map[string]*SpiderStatus
 }
 
 func NewCrawlerStatus() *CrawlerStatus {
@@ -39,27 +39,27 @@ func NewCrawlerStatus() *CrawlerStatus {
 }
 
 func (this *CrawlerStatus) IsSpiderRunning(spiderName string) bool {
-	_, ok := this.runningSpider[spiderName]
+	_, ok := this.RunningSpider[spiderName]
 	return ok
 }
 
 // add a spider to running map
 func (this *CrawlerStatus) StartSpider(spiderName string) {
-	_, ok := this.runningSpider[spiderName]
+	_, ok := this.RunningSpider[spiderName]
 	if !ok {
-		this.runningSpider[spiderName] = NewSpiderStatus(spiderName)
+		this.RunningSpider[spiderName] = NewSpiderStatus(spiderName)
 	}
 }
 
 // add a request to wait in spiderName
 func (this *CrawlerStatus) Push(spiderName string) {
-	this.runningSpider[spiderName].Waiting += 1
+	this.RunningSpider[spiderName].Waiting += 1
 }
 
 // if cluster distribute a request,
 // waiting -1 runing +1
 func (this *CrawlerStatus) Distribute(spiderName string) {
-	spiderStatus, _ := this.runningSpider[spiderName]
+	spiderStatus, _ := this.RunningSpider[spiderName]
 	spiderStatus.Waiting -= 1
 	spiderStatus.Running += 1
 }
@@ -67,14 +67,14 @@ func (this *CrawlerStatus) Distribute(spiderName string) {
 // get crawl result
 // runing -1 crawled +1
 func (this *CrawlerStatus) Crawled(spiderName string) {
-	spiderStatus, _ := this.runningSpider[spiderName]
+	spiderStatus, _ := this.RunningSpider[spiderName]
 	spiderStatus.Running -= 1
 	spiderStatus.Crawled += 1
 }
 
 // judge a is a spider can stop
 func (this *CrawlerStatus) CanWeStop(spiderName string) bool {
-	spiderStatus, _ := this.runningSpider[spiderName]
+	spiderStatus, _ := this.RunningSpider[spiderName]
 	leftNum := spiderStatus.Running + spiderStatus.Waiting
 	return leftNum <= 0
 }
@@ -83,9 +83,9 @@ func (this *CrawlerStatus) CanWeStop(spiderName string) bool {
 // remove from runningSpider
 // add to crawledSpider
 func (this *CrawlerStatus) CloseSpider(spiderName string) *SpiderStatus {
-	spiderStatus, _ := this.runningSpider[spiderName]
+	spiderStatus, _ := this.RunningSpider[spiderName]
 	spiderStatus.EndTime = time.Now()
-	this.crawledSpider = append(this.crawledSpider, spiderStatus)
-	delete(this.runningSpider, spiderName)
+	this.CrawledSpider = append(this.CrawledSpider, spiderStatus)
+	delete(this.RunningSpider, spiderName)
 	return spiderStatus
 }
